108dc48b4c277f463b69ce1a4a061df464258294
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-12-26 18:17:29 +0800

    Adds ship and boardInit factory functions
    ship factory creates ship object to track hits and length, boardInit
    contains incomplete tracking of ships on designated coordinate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
 import './style.css';
 
-function tester() {}
+function ship(long) {
+  let length = long;
+  let hits = 0;
 
-export { tester };
+  function hit() {
+    hits++;
+    isSunk();
+  }
+
+  function isSunk() {
+    return hits >= length;
+  }
+
+  return { length, hit, isSunk };
+}
+
+function boardInit() {
+  let board = [];
+  let shipPlacement = [];
+
+  for (let i = 0; i < 10; i++) {
+    board.push([]);
+    for (let j = 0; j < 10; j++) {
+      board[i].push([]);
+    }
+  }
+
+  function fullCoordinate(start, end) {
+    let fullCoordinates = [];
+    if (start[0] == end[0]) {
+      for (let i = start[1]; i <= end[1]; i++) {
+        fullCoordinates.push([start[0], i]);
+      }
+    }
+    if (start[1] == end[1]) {
+      for (let i = start[0]; i <= end[0]; i++) {
+        fullCoordinates.push([i, start[1]]);
+      }
+    }
+    return fullCoordinates;
+  }
+
+  function placementAdder(start, end) {}
+
+  function collisionChecker(start, end) {}
+
+  function placeShip(start, end, length) {
+    shipPlacement.push([start, end]);
+    board[start[0]][start[1]].push(ship(length));
+    if (start[0] == end[0]) {
+      for (let i = start[1] + 1; i <= end[1]; i++) {
+        board[start[0]][start[i]].push();
+      }
+    }
+  }
+
+  return { fullCoordinate };
+}
+
+export { ship, boardInit };
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,18 @@
-import { tester } from './index.js';
+import { experiments } from 'webpack';
+import { ship, boardInit } from './index.js';
 
-test('setup test', () => {
-  expect(tester).toBeDefined();
+let shipT = ship(1);
+let boardT = boardInit();
+
+test('Initialize ship with length', () => {
+  expect(shipT.length).toBe(1);
+});
+
+test('Battleship sunk', () => {
+  shipT.hit();
+  expect(shipT.isSunk()).toBe(true);
+});
+
+test('Place ship', () => {
+  expect(boardT.fullCoordinate([0, 3], [0, 6])).toBe(true);
 });
